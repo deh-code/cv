4067cf468268ed1f0da4cd1e462848400abd6993
Ruben Fileti <rbnflt3@gmail.com>
2024-07-01 11:42:27 +0200

    Add education section to sidebar

--- src/sections/sidebar.jsx
+++ src/sections/sidebar.jsx
@@ -3,9 +3,10 @@ import Container from "../components/container";
 import profile from '../assets/images/profile.jpeg'
 import git from '../assets/images/github-mark.png';
 import linkedin from '../assets/images/LI-In-Bug.png'
-import { faEnvelope, faLocationDot, faMobileScreen } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope, faGraduationCap, faLocationDot, faMobileScreen } from "@fortawesome/free-solid-svg-icons";
 import Icon from "../components/icon";
 import Row from "../components/row";
+import Title from "../components/title";
 
 const links = [
   {
@@ -37,94 +38,156 @@ export default function Sidebar() {
       paddingHorizontal: '12px'
     }}
   >
-    <Text
-      style={{
-        fontSize: '28px',
-        fontWeight: 'bold',
-        marginBottom: '16px'
-      }}
-    >Ruben Fileti</Text>
-    <Row
-      style={{
-        alignItems: 'stretch',
-        marginBottom: '16px'
-      }}>
-      <View
-        style={{
-          width: '50%',
-          paddingRight: '6px'
-        }}>
-          <Image src={profile} style={{
-            borderRadius: '8px'
-          }}></Image>
-        </View>
-      <View 
-        style={{
-          width: '50%',
-          paddingLeft: '6px'
-        }}>
-          <Text
-            style={{
-              fontSize: '15px',
-              fontWeight: 'semibold'
-            }}
-          >Fullstack</Text>
-          <Text
-            style={{
-              fontSize: '15px',
-              fontWeight: 'semibold',
-            }}
-          >Developer</Text>
-          <Row 
-            style={{
-              fontSize: '11px',
-              color: '#666',
-              marginTop: '16px'
-            }}
-          >
-            <Icon fill='#666' width='10px' style={{marginRight: '2px'}} faIcon={faLocationDot}></Icon>
-            <Text
-              style={{
-                fontStyle: 'italic',
-                fontWeight: 'medium'
-              }}>
-                Pisa, Italy
-              </Text>
-          </Row>
-      </View>
-    </Row>
     <View
       style={{
-        marginBottom: '10px',
-        fontSize: '11px',
-        color: '#666'
+        borderBottom: '1px solid #eee',
+        paddingBottom: '16px',
+        marginBottom: '16px'
       }}>
-        {contacts.map((contact, index) => 
-          <Row key={index} style={{marginBottom: '8px'}}>
-            <Icon fill='#666' width='10px' style={{marginRight: '3px'}} faIcon={contact.icon}></Icon>
-            <Text>{contact.text}</Text>
-          </Row>
-        )}
-    </View>
-    <Row>
-        {links.map((link, index) =>  
-          <Link
-            key={index}
-            href={link.href}
-            style={{
-              display: "flex",
-              flexDirection: 'row',
-              alignItems: 'center',
-              fontSize: '11px',
-              fontWeight: 'medium',
-              color: 'black',
-              textDecoration: 'none',
-              marginRight: '16px',
-            }}>
-            <Image style={{width: '14px', marginRight: '2px'}} src={link.icon}/>
-            <Text>{link.title}</Text>
-          </Link>
-        )}
+      <Text
+        style={{
+          fontSize: '28px',
+          fontWeight: 'bold',
+          marginBottom: '16px'
+        }}
+      >Ruben Fileti</Text>
+      <Row
+        style={{
+          alignItems: 'stretch',
+          marginBottom: '16px'
+        }}>
+        <View
+          style={{
+            width: '50%',
+            paddingRight: '6px'
+          }}>
+            <Image src={profile} style={{
+              borderRadius: '8px'
+            }}></Image>
+          </View>
+        <View 
+          style={{
+            width: '50%',
+            paddingLeft: '6px'
+          }}>
+            <Text
+              style={{
+                fontSize: '15px',
+                fontWeight: 'semibold'
+              }}
+            >Fullstack</Text>
+            <Text
+              style={{
+                fontSize: '15px',
+                fontWeight: 'semibold',
+              }}
+            >Developer</Text>
+            <Row 
+              style={{
+                fontSize: '11px',
+                color: '#666',
+                marginTop: '16px'
+              }}
+            >
+              <Icon fill='#666' width='8px' style={{marginRight: '2px'}} faIcon={faLocationDot}></Icon>
+              <Text
+                style={{
+                  fontStyle: 'italic',
+                  fontWeight: 'medium',
+                  fontSize: '10px'
+                }}>
+                  Pisa, Italy
+                </Text>
+            </Row>
+        </View>
       </Row>
+      <View
+        style={{
+          marginBottom: '10px',
+          fontSize: '11px',
+          color: '#666'
+        }}>
+          {contacts.map((contact, index) => 
+            <Row key={index} style={{marginBottom: '8px'}}>
+              <Icon fill='#666' width='10px' style={{marginRight: '3px'}} faIcon={contact.icon}></Icon>
+              <Text>{contact.text}</Text>
+            </Row>
+          )}
+      </View>
+      <Row>
+          {links.map((link, index) =>  
+            <Link
+              key={index}
+              href={link.href}
+              style={{
+                display: "flex",
+                flexDirection: 'row',
+                alignItems: 'center',
+                fontSize: '11px',
+                fontWeight: 'medium',
+                color: 'black',
+                textDecoration: 'none',
+                marginRight: '16px',
+              }}>
+              <Image style={{width: '14px', marginRight: '2px'}} src={link.icon}/>
+              <Text>{link.title}</Text>
+            </Link>
+          )}
+        </Row>
+    </View>
+    <View>
+      <Row>
+      <Icon
+        faIcon={faGraduationCap}
+        style={{
+          marginRight: '4px'
+        }}></Icon>
+      <Title
+        style={{
+          fontSize: '14px',
+          marginBottom: '8px'
+        }}>
+        Education
+      </Title>
+      <Row
+        style={{
+          fontSize: '9px',
+          marginBottom: '4px'
+        }}>
+        <Text
+          style={{
+            fontWeight: 'bold'
+          }}>
+          Bachelor&apos;s degree 
+        </Text>
+        <Text style={{marginHorizontal: '2px'}}>-</Text>
+        <Text
+          style={{
+            fontWeight: 'bold',
+            fontStyle: 'italic'
+          }}>
+          Computer Science 
+        </Text>
+      </Row>
+      <Row>
+        <Icon
+          faIcon={faLocationDot} 
+          width='8px' 
+          fill='#666' 
+          style={{
+            marginRight: '2px'
+          }}></Icon>
+        <Text
+          style={{
+            fontWeight: 'semibold',
+            fontSize: '9px',
+            color: '#666',
+            fontStyle: 'italic'
+          }}>
+          University of Pisa
+        </Text>
+      </Row>
+      </Row>
+    </View>
   </Container>
 }
